2bf349366418c5589688246eb48b39bc991a3c6c
Xavier Villavicencio <jxavier2525@gmail.com>
2024-12-16 01:30:40 -0500

    all register Items and blocks, alpha wordlgen

---
+++ neoforge/src/main/java/com/graclyxz/tinoresandcrafts/init/ModItems.java
@@ -0,0 +1,91 @@
+package com.graclyxz.tinoresandcrafts.init;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.equipment.ArmorMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static com.graclyxz.tinoresandcrafts.Constants.MODID;
+import static com.graclyxz.tinoresandcrafts.init.ModMaterials.*;
+
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+
+    /*-*-*-*-*-*-*-*-* Item and Blocks creation *-*-*-*-*-*-*-*-*/
+    public static final List<DeferredItem<Item>> TIN_ITEMS = registerAllItems("tin", Tool.TIN, Armor.TIN, new Item.Properties());
+    public static final List<DeferredBlock<Block>> TIN_BLOCKS = registerAllBlocks("tin",  new float[]{4f, 6f}, SoundType.STONE);
+
+    /*-*-*-*-*-*-*-*-* item and blocks registration *-*-*-*-*-*-*-*-*/
+    public static DeferredItem<Item> registerItem(String name, Function<Item.Properties, Item> function, Item.Properties itemProp) {
+        return ITEMS.register(name, () -> function.apply(itemProp.setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MODID, name)))));
+    }
+
+    public static DeferredBlock<Block> registerBlock(String name, Function<BlockBehaviour.Properties, Block> function, BlockBehaviour.Properties blockProp, Item.Properties itemProp) {
+        var blockReg = registerBlock(name, function, blockProp);
+        registerItem(name, (p) -> new BlockItem(blockReg.get(), p), itemProp.useBlockDescriptionPrefix());
+        return blockReg;
+    }
+
+    public static DeferredBlock<Block> registerBlock(String name, Function<BlockBehaviour.Properties, Block> function, BlockBehaviour.Properties blockProp) {
+        return BLOCKS.register(name, () -> function.apply(blockProp.setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(MODID, name)))));
+    }
+
+    /*-*-*-*-*-*-*-*-* Items list *-*-*-*-*-*-*-*-*/
+    private static List<DeferredItem<Item>> registerAllItems(String name, ToolMaterial toolmaterial, ArmorMaterial armormaterial, Item.Properties itemProp) {
+        return List.of(
+                registerItem( "raw_" + name, Item::new, itemProp),
+                registerItem( name +"_ingot", Item::new, itemProp),
+                registerItem( name +"_nugget", Item::new, itemProp),
+
+                registerItem(name + "_sword", (p) -> new SwordItem(toolmaterial,  5, -2.4f, p), itemProp),
+                registerItem(name + "_pickaxe", (p) -> new PickaxeItem(toolmaterial,  3, -2.8f, p), itemProp),
+                registerItem(name + "_axe", (p) -> new AxeItem(toolmaterial,  8, -3.1f, p), itemProp),
+                registerItem(name + "_hoe", (p) -> new HoeItem(toolmaterial,  0, -1f, p), itemProp),
+                registerItem( name + "_shovel", (p) -> new ShovelItem(toolmaterial,  3.5f, -3f, p), itemProp),
+
+                registerItem(name + "_helmet", (p) -> new ArmorItem(armormaterial, ArmorType.HELMET, p), itemProp),
+                registerItem(name + "_chestplate", (p) -> new ArmorItem(armormaterial, ArmorType.CHESTPLATE, p), itemProp),
+                registerItem(name + "_leggings", (p) -> new ArmorItem(armormaterial, ArmorType.LEGGINGS, p), itemProp),
+                registerItem(name + "_boots", (p) -> new ArmorItem(armormaterial, ArmorType.BOOTS, p), itemProp)
+        );
+    }
+
+    /*-*-*-*-*-*-*-*-* Blocks list *-*-*-*-*-*-*-*-*/
+    private static List<DeferredBlock<Block>> registerAllBlocks(String name, float[] strengthattr, SoundType soundblock) {
+        return List.of(
+                registerBlock(name + "_block", Block::new, BlockBehaviour.Properties.of().requiresCorrectToolForDrops()
+                        .strength(4f, 6f).sound(SoundType.METAL),
+                        new Item.Properties()),
+
+                registerBlock(name + "_ore", Block::new, BlockBehaviour.Properties.of().requiresCorrectToolForDrops()
+                                .strength(strengthattr[0],strengthattr[1]).sound(soundblock),
+                        new Item.Properties()),
+
+                registerBlock("raw_" + name + "_block", Block::new, BlockBehaviour.Properties.of().requiresCorrectToolForDrops()
+                                .strength(4f, 6f).sound(SoundType.STONE),
+                        new Item.Properties())
+        );
+    }
+
+    public static void init(IEventBus bus) {
+        ITEMS.register(bus);
+        BLOCKS.register(bus);
+    }
+}
---
+++ neoforge/src/main/java/com/graclyxz/tinoresandcrafts/init/ModTab.java
@@ -0,0 +1,33 @@
+package com.graclyxz.tinoresandcrafts.init;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+import static com.graclyxz.tinoresandcrafts.Constants.MODID;
+
+
+public class ModTab {
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final Supplier<CreativeModeTab> TAB_TIN_ORES_AND_CRAFTS = TABS.register("tab_tinoresandcrafts", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.TIN_ITEMS.get(1).asItem()))
+            .displayItems((features, event) -> {
+                for (DeferredHolder<Item, ? extends Item> item : ModItems.ITEMS.getEntries())
+                    event.accept(item.get());
+            })
+            .title(Component.translatable("item_group.tinoresandcrafts.tab_tinoresandcrafts"))
+            .build());
+
+
+    public static void init(IEventBus bus) {
+        TABS.register(bus);
+    }
+}
