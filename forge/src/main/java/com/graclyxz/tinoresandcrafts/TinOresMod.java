3ff048e11b62b8f3e4a3bf7da2f5e0f26438f897
Xavier Villavicencio <jxavier2525@gmail.com>
2024-12-18 12:49:16 -0500

    Forge version

--- forge/src/main/java/com/graclyxz/tinoresandcrafts/TinOresMod.java
+++ forge/src/main/java/com/graclyxz/tinoresandcrafts/TinOresMod.java
@@ -1,12 +1,17 @@
 package com.graclyxz.tinoresandcrafts;
 
+import com.graclyxz.tinoresandcrafts.init.ModItems;
+import com.graclyxz.tinoresandcrafts.init.ModTab;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MODID)
 public class TinOresMod {
 
-    public TinOresMod() {
+    public TinOresMod(FMLJavaModLoadingContext context) {
 
+        IEventBus bus = context.getModEventBus();
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
@@ -14,6 +19,8 @@ public class TinOresMod {
         // Use Forge to bootstrap the Common mod.
         Constants.LOG.info("Hello Forge world!");
         CommonClass.init();
+        ModItems.init(bus);
+        ModTab.init(bus);
 
     }
 }
